43c0153f97a8f742c22f06a088a784da5214e468
SallyLueNoa <sally.lue.noa@gmail.com>
2022-11-12 22:50:24 +0900

    [Feature implementation] Create android project. #1
    * Change Java version and Kotlin JVM target to 1.8 in buildSrc.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,10 +11,10 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "1.8"
 }
